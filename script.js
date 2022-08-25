5af2f356ef7ee1dee726720d8f14b9e0296b2bbe
Leo <95043714+leo-lungu@users.noreply.github.com>
2022-08-25 11:23:24 +0100

    Cleaned indentation

--- script.js
+++ script.js
@@ -1,84 +1,84 @@
 let DISPLAY = "";
 let CURRENT_OPERATOR = "";
-let FIRST_NUMBER = "";  
+let FIRST_NUMBER = "";
 let SECOND_NUMBER = "";
 
 function add(a, b) {
-    return parseFloat(a) + parseFloat(b);
+  return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a, b) {
-    return(a - b);
+  return (a - b);
 }
 
 function multiply(a, b) {
-    return (a * b);
+  return (a * b);
 }
 
 function divide(a, b) {
-    return (a / b);
+  return (a / b);
 }
 
 function operate(operator, a, b) {
-      if (operator === "add") {
-        DISPLAY = add(a, b);
-      } else if (operator === "subtract") {
-        DISPLAY = subtract(a, b);
-      } else if (operator === "multiply") {
-        DISPLAY = multiply(a, b);
-      } else if (operator === "divide") {
-        DISPLAY = divide(a, b);
-      } else if (operator === "mod") {
-        DISPLAY = b / 100;
-      } 
-      document.getElementById("answer").innerHTML = DISPLAY;
-      FIRST_NUMBER = DISPLAY;
-      console.log("first display: " + FIRST_NUMBER)
+  if (operator === "add") {
+    DISPLAY = add(a, b);
+  } else if (operator === "subtract") {
+    DISPLAY = subtract(a, b);
+  } else if (operator === "multiply") {
+    DISPLAY = multiply(a, b);
+  } else if (operator === "divide") {
+    DISPLAY = divide(a, b);
+  } else if (operator === "mod") {
+    DISPLAY = b / 100;
+  }
+  document.getElementById("answer").innerHTML = DISPLAY;
+  FIRST_NUMBER = DISPLAY;
+  console.log("first display: " + FIRST_NUMBER)
 }
 
 function numberPushed(id) {
-    number = document.getElementById(id).innerHTML;
-    DISPLAY = DISPLAY + number;
-    console.log(DISPLAY);
-    document.getElementById("answer").innerHTML = DISPLAY;
+  number = document.getElementById(id).innerHTML;
+  DISPLAY = DISPLAY + number;
+  console.log(DISPLAY);
+  document.getElementById("answer").innerHTML = DISPLAY;
 }
 
 function operatorPushed(id) {
-    console.log("Current Operator: " + CURRENT_OPERATOR);
-    if (id == "mod") {
-      CURRENT_OPERATOR = id;
+  console.log("Current Operator: " + CURRENT_OPERATOR);
+  if (id == "mod") {
+    CURRENT_OPERATOR = id;
+    operate(CURRENT_OPERATOR, FIRST_NUMBER, DISPLAY);
+    CURRENT_OPERATOR = "";
+    document.getElementById("answer").innerHTML = DISPLAY;
+    console.log(DISPLAY);
+  }
+  else {
+    if (CURRENT_OPERATOR !== "equals" && id !== "mod") {
+      console.log("testoperator");
+      console.log("FIRST: " + FIRST_NUMBER);
+      console.log("DISPLAY: " + DISPLAY);
       operate(CURRENT_OPERATOR, FIRST_NUMBER, DISPLAY);
-      CURRENT_OPERATOR = "";
-      document.getElementById("answer").innerHTML = DISPLAY;
-      console.log(DISPLAY);
-    }
-    else {
-      if (CURRENT_OPERATOR !== "equals" && id !== "mod") {
-        console.log("testoperator");
-        console.log("FIRST: " + FIRST_NUMBER);
-        console.log("DISPLAY: " + DISPLAY);
-        operate(CURRENT_OPERATOR, FIRST_NUMBER, DISPLAY);
-        DISPLAY = "";
-        SECOND_NUMBER = "";
-        CURRENT_OPERATOR = id;
+      DISPLAY = "";
+      SECOND_NUMBER = "";
+      CURRENT_OPERATOR = id;
     } else if (id == "mod") {
       CURRENT_OPERATOR = id;
       operate(CURRENT_OPERATOR, FIRST_NUMBER, DISPLAY);
     } else {
-        FIRST_NUMBER = DISPLAY;
-        console.log("testnonoperator");
-        CURRENT_OPERATOR = document.getElementById(id).id;
-        DISPLAY = "";
-        console.log(id);
+      FIRST_NUMBER = DISPLAY;
+      console.log("testnonoperator");
+      CURRENT_OPERATOR = document.getElementById(id).id;
+      DISPLAY = "";
+      console.log(id);
     }
-    }
+  }
 }
 
 function equalsPushed(id) {
-      operate(CURRENT_OPERATOR, FIRST_NUMBER, DISPLAY);
-      CURRENT_OPERATOR = id;
-      document.getElementById("answer").innerHTML = DISPLAY;
-      console.log(DISPLAY);
+  operate(CURRENT_OPERATOR, FIRST_NUMBER, DISPLAY);
+  CURRENT_OPERATOR = id;
+  document.getElementById("answer").innerHTML = DISPLAY;
+  console.log(DISPLAY);
 }
 
 function resetDisplay() {
